69398d4f6c498a6577fffcbbffb0604a7c7dd77e
Yussuf Abdi <yussufabdi@Yussufs-MacBook-Pro.local>
2026-01-16 14:17:01 -0600

    Fix: Update Stripe API version to 2025-12-15.clover

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-05-28.basil",
+  apiVersion: "2025-12-15.clover",
   typescript: true,
 });
 
